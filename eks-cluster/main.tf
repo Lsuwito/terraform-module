b0fd2968cca7ddca9890b229b3b79b8e39980b69
Lom Suwito <lomintun@gmail.com>
2021-06-06 18:56:40 -0700

    Fix outgoing internet route cidr block value

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -16,7 +16,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "::/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
 }
@@ -36,7 +36,7 @@ resource "aws_nat_gateway" "nat_gateway" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block     = "::/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
 }
